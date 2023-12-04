d42235a87f9635fed6a01d3e30dd24b2d797af27
username <yubyeong01@naver.com>
2023-12-04 17:30:46 +0900

    9th

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,20 @@
 import TopicsList from '@/components/TopicsList'
-import Image from 'next/image'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect('/signIn')
+  }
+
   return (
     <div>
-      <h1 className='text-3xl font-bold'>Recent News</h1>
+      <h1 className="text-3xl font-bold">Recent News</h1>
       <p>MongoDB CRUD example</p>
       <p>Next-auth 인증</p>
-      {<TopicsList/>}
+      <TopicsList />
     </div>
   )
 }
